3a682fb1557bc5cb1571d6a1d7aead639e57e675
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-05-07 21:42:15 +0300

    Added basics

---
+++ l04/comments.php
@@ -0,0 +1,20 @@
+<?php
+
+// echo 123;
+# echo 123;
+/*
+echo 123;
+echo 123;
+*/
+
+echo 123/*131231*/, 11;
+
+/**
+ * Function for checking some data
+ * @param string $id Id of user
+ * @return array<int, string> Data of current user
+ */
+function test (string $id): array
+{
+    return [];
+}
---
+++ l04/variables.php
@@ -0,0 +1,20 @@
+<?php
+
+$fruit = 'apple';
+$fruit = 'banana';
+
+echo $fruit, '<br>';
+
+//$1test = 1;
+$te2st3 = 1;
+//$re<re = 2;
+$еуые = 22;
+echo $еуые, '<br>';
+$_test_rrr = 123;
+echo $_test_rrr, '<br>';
+
+$my_little_pony = 'Shimmershine';
+$myLittlePony = 'Rainbow Dash';
+$mylittlepony = 'Horse';
+
+echo $my_little_pony, '<br>', $myLittlePony, '<br>', $mylittlepony;
