8bd15e4f424a5c7e432c8582326588463d0ebe9a
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-06-09 21:44:41 +0300

    L13: Added directories manager

---
+++ l13/breadcrumbs.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @var string $rout (From index.php)
+ */
+
+?>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">
+            <?= $rout ?: '/' ?>
+        </li>
+    </ol>
+</nav>
---
+++ l13/error.php
@@ -0,0 +1,5 @@
+<?php if (isset($_GET['error_message'])): ?>
+<div class="alert alert-danger" role="alert">
+    <?= $_GET['error_message'] ?>
+</div>
+<?php endif; ?>
