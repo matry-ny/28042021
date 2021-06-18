a4f57bcd4ed9f1c8a055b17bb943df0b7ef4aba2
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-06-18 19:55:00 +0300

    L16: Added cookies and sessions demonstrations

---
+++ l16/cookies.php
@@ -0,0 +1,6 @@
+<?php
+
+setcookie('test', 123);
+//setcookie('temp', 'qwerty', time() + 30);
+//setcookie('test', null, -1);
+var_dump($_COOKIE);
---
+++ l16/sessions.php
@@ -0,0 +1,14 @@
+<?php
+
+//ini_set('session.gc_maxlifetime', 5);
+
+session_start();
+
+//$_SESSION['test'] = 123123;
+//$_SESSION['qqqqqq'] = [1, 2, 3];
+
+//$_SESSION['rrr'] = 0;
+
+var_dump($_SESSION);
+
+phpinfo();
